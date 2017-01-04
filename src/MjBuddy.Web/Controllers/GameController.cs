89e649cb1c43ebd44fd3726df9d83fd7396fc269
Tonny Thiatmaja <tthiatma@gmail.com>
2017-01-04 01:34:00 -0700

    added game controller

---
+++ src/MjBuddy.Web/Controllers/GameController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MjBuddy.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        public GameController()
+        {
+
+        }
+        /// <summary>
+        /// User will wait in lobby until game is ready
+        /// </summary>
+        /// <returns></returns>
+        public ViewResult Lobby()
+        {
+            return View();
+        }
+        public ViewResult GameRoom()
+        {
+            return View();
+        }
+
+    }
+}
